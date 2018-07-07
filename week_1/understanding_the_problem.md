041bf58977f5f80ca30181cb90619a92a5ae9455
fabiorcampos <fabiorochacampos@gmail.com>
2018-07-07 12:21:43 -0300

    upload data

---
+++ week_1/understanding_the_problem.md
@@ -0,0 +1,31 @@
+The first step in analyzing any new data set is figuring out: (a) what data you have and (b) what are the standard tools and models used for that type of data. Make sure you have downloaded the data from Coursera before heading for the exercises. This exercise uses the files named LOCALE.blogs.txt where LOCALE is the each of the four locales en\_US, de\_DE, ru\_RU and fi\_FI. The data is from a corpus called HC Corpora. See the About the Corpora reading for more details. The files have been language filtered but may still contain some foreign text.
+
+In this capstone we will be applying data science in the area of natural language processing. As a first step toward working on this project, you should familiarize yourself with Natural Language Processing, Text Mining, and the associated tools in R. Here are some resources that may be helpful to you.
+
+Natural language processing Wikipedia page Text mining infrastucture in R CRAN Task View: Natural Language Processing Coursera course on NLP (not in R)
+
+###### Dataset
+
+This is the training data to get you started that will be the basis for most of the capstone. You must download the data from the Coursera site and not from external websites to start.
+
+###### Capstone Dataset
+
+Your original exploration of the data and modeling steps will be performed on this data set. Later in the capstone, if you find additional data sets that may be useful for building your model you may use them.
+
+###### Tasks to accomplish
+
+Obtaining the data - Can you download the data and load/manipulate it in R? Familiarizing yourself with NLP and text mining - Learn about the basics of natural language processing and how it relates to the data science process you have learned in the Data Science Specialization.
+
+###### Questions to consider
+
+-   What do the data look like?
+
+-   Where do the data come from?
+
+-   Can you think of any other data sources that might help you in this project?
+
+-   What are the common steps in natural language processing?
+
+-   What are some common issues in the analysis of text data?
+
+-   What is the relationship between NLP and the concepts you have learned in the Specialization?
---
+++ week_2/exploratory.R
@@ -0,0 +1,93 @@
+library(stringi)
+library(tm)
+library(ngram)
+library(ggplot2)
+library(RWeka)
+library(SnowballC)
+
+### load files
+news <- readLines("C:/Users/fcampos/OneDrive - Cisa Trading/Documents/Estudos/Coursera_Spec/datascience/capstone-data-science/capstone-data-science/data/en_US/en_US.news.txt", encoding = "UTF-8", skipNul = TRUE)
+blogs <- readLines("C:/Users/fcampos/OneDrive - Cisa Trading/Documents/Estudos/Coursera_Spec/datascience/capstone-data-science/capstone-data-science/data/en_US/en_US.blogs.txt", encoding = "UTF-8", skipNul = TRUE)
+twitter <- readLines("C:/Users/fcampos/OneDrive - Cisa Trading/Documents/Estudos/Coursera_Spec/datascience/capstone-data-science/capstone-data-science/data/en_US/en_US.twitter.txt", encoding = "UTF-8", skipNul = TRUE)
+
+### Information of files
+
+#### Size in MB
+nsize <- file.info("C:/Users/fcampos/OneDrive - Cisa Trading/Documents/Estudos/Coursera_Spec/datascience/capstone-data-science/capstone-data-science/data/en_US/en_US.news.txt")$size / 1024 ^ 2
+bsize <- file.info("C:/Users/fcampos/OneDrive - Cisa Trading/Documents/Estudos/Coursera_Spec/datascience/capstone-data-science/capstone-data-science/data/en_US/en_US.blogs.txt")$size / 1024 ^ 2
+tsize <- file.info("C:/Users/fcampos/OneDrive - Cisa Trading/Documents/Estudos/Coursera_Spec/datascience/capstone-data-science/capstone-data-science/data/en_US/en_US.twitter.txt")$size / 1024 ^ 2
+totalsize <- nsize + bsize + tsize
+
+#### Length Lines 
+nlines <- length(news)
+blines <- length(blogs)
+tlines <- length(twitter)
+totallength <- nlines + blines + tlines
+
+#### Longest line in each file
+nmax <- max(stri_count_words(news))
+bmax <- max(stri_count_words(blogs))
+tmax <- max(stri_count_words(twitter))
+totalmax <- nmax + bmax + tmax
+
+### length(grep("love", twitter)) / length(grep("hate", twitter))
+
+#### Mean words in each file
+nmean <- mean(stri_count_words(news))
+bmean <- mean(stri_count_words(blogs))
+tmean <- mean(stri_count_words(twitter))
+meantotal <- nmean + bmean + tmean / 3
+
+names <- c("news", "blogs", "twitter")
+
+dfsummary <- data.frame(sizeinmb = c(nsize, bsize, tsize),
+                        lengthlines = c(nlines, blines, tlines),
+                        longestline = c(nmax, bmax, tmax),
+                        meanwords = c(nmean, bmean, tmean),
+                        total = c(totalsize, totallength, totalmax), row.names = names)
+print(dfsummary)
+
+### Create a sample file
+set.seed(5150)
+sample <- c(sample(news, length(news) * .005),
+            sample(blogs, length(blogs) * 005),
+            sample(twitter, length(twitter) * .005))
+
+
+### Clean and organize data
+sample <- iconv(sample, 'UTF-8', 'ASCII')
+mycorpus <- VCorpus(VectorSource(sample))
+toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+mycorpus <- tm_map(mycorpus, toSpace, "(f|ht)tp(s?)://(.*)[.][a-z]+")
+mycorpus <- tm_map(mycorpus, toSpace, "@[^\\s]+")
+mycorpus <- tm_map(mycorpus, tolower)
+mycorpus <- tm_map(mycorpus, function(x) iconv(enc2utf8(x), sub = "byte"))
+mycorpus <- tm_map(mycorpus, removeWords, stopwords("english"))
+mycorpus <- tm_map(mycorpus, stemDocument)
+mycorpus <- tm_map(mycorpus, removeNumbers)
+mycorpus <- tm_map(mycorpus, stripWhitespace)
+mycorpus <- tm_map(mycorpus, PlainTextDocument)
+
+
+### Create function to n-grams
+getFreq <- function(tdm) {
+      freq <- sort(rowSums(as.matrix(tdm)), decreasing = TRUE)
+      return(data.frame(word = names(freq), freq = freq))
+}
+bigram <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+trigram <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+makePlot <- function(data, label) {
+      ggplot(data[1:30,], aes(reorder(word, -freq), freq)) +
+            labs(x = label, y = "Frequency") +
+            theme(axis.text.x = element_text(angle = 60, size = 12, hjust = 1)) +
+            geom_bar(stat = "identity", fill = I("red"))
+}
+
+freq1 <- getFreq(removeSparseTerms(TermDocumentMatrix(mycorpus), 0.9999))
+freq2 <- getFreq(removeSparseTerms(TermDocumentMatrix(mycorpus, control = list(tokenize = bigram)), 0.9999))
+freq3 <- getFreq(removeSparseTerms(TermDocumentMatrix(mycorpus, control = list(tokenize = trigram)), 0.9999))
+
+makePlot(freq1, "30 Most Common Unigrams")
+makePlot(freq2, "30 Most Common Bigrams")
+makePlot(freq3, "30 Most Common Trigrams")
+
